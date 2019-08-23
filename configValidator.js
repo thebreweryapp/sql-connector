9bdea8f6db910cb901267cec067cc192d5864543
Josh Mante <jmante@Josh-Macbook-Pro.local>
2019-08-23 17:42:54 +0800

    initial commit

---
+++ configValidator.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {Object} config
+ * @return {Object} validation results
+ */
+const configValidator = (config) => {
+  return {
+    isValid: true
+  };
+};
+
+module.exports = configValidator;
---
+++ index.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const { connectorFactory } = require('brewery-core');
+const configValidator = require('./configValidator');
+
+/**
+ * 
+ * @param {Object} config Connector config object
+ */
+const initialize = (config) => {
+  /** validate config input */
+  const { isValid, errors } = configValidator(config);
+  if(!isValid) {
+    throw new Error('Invalid config');
+  }
+
+  this.sequelize = new Sequelize(config);
+  this.sequelize.DataTypes = Sequelize.DataTypes; 
+  this.sequelize.isSync = config.sync;
+  /** create sequelize instance */
+  return this.sequelize;
+};
+
+/**
+ * Connect to service
+ */
+const connect = () => {
+  return this.sequelize.authenticate();
+};
+
+/**
+ * Disconnect from service
+ */
+const disconnect = () => {
+  return this.sequelize.close();
+};
+
+
+const SqlConnector = connectorFactory('sql', initialize, connect, disconnect);
+
+module.exports = SqlConnector;
