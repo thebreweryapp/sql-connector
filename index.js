cba679c8f386bfd28c85d02d20f515be988ced5a
Josh Mante <jmante@stratpoint.com>
2019-10-31 17:18:31 +0800

    added alter columns when sync on config

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const { connectorFactory } = require('@amberjs/core');
 const configValidator = require('./configValidator');
 
 /**
- * 
+ *
  * @param {Object} config Connector config object
  */
-const initialize = (config) => {
+const initialize = config => {
   /** validate config input */
   const { isValid, errors } = configValidator(config);
-  if(!isValid) {
+  if (!isValid) {
     console.log(errors);
     throw new Error('Invalid config');
   }
 
   this.sequelize = new Sequelize(config);
-  this.sequelize.DataTypes = Sequelize.DataTypes; 
+  this.sequelize.DataTypes = Sequelize.DataTypes;
   this.sequelize.isSync = config.isSync;
+  this.sequelize.alter = config.alter;
   /** create sequelize instance */
   return this.sequelize;
 };
@@ -34,7 +35,6 @@ const connect = () => {
 const disconnect = () => {
   return this.sequelize.close();
 };
-
 
 const SqlConnector = connectorFactory('sql', initialize, connect, disconnect);
 
